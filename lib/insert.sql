ec91885e4f185321d0b21b0f58db24d94998cb38
Ruth <rwangare124@gmail.com>
2023-05-27 19:27:10 +0300

    Completd assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 18, "M", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 18, "M", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 18, "F", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 18, "M", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 18, "F", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 18, "M", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 18, "F", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 18, "M", "Brown", "moderate", TRUE);
